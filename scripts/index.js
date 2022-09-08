b3eba2095b97fde704567386c6148f855b8e753d
Masha Soldatova <threebadwolves@gmail.com>
2022-09-09 02:01:31 +0300

    bugfix: исправить попап

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,31 @@
 let formElement = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-icon');
-
-editButton.addEventListener('click', function(e) {
-  e.preventDefault()
-  formElement.classList.add('popup_opened')
-});
-
-closeButton.addEventListener('click', function() {
-  formElement.classList.remove('popup_opened');
-});
-
-// ↑ выскакивание окошка
-
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__description');
 let nameInput = document.querySelector('.popup__input_line_name');
 let jobInput = document.querySelector('.popup__input_line_description');
 let submitButton = document.querySelector('.popup__submit');
 
+function openPopup() {
+  formElement.classList.add('popup_opened');
+  let nameInputV = document.querySelector('.popup__input_line_name').value = profileName.textContent;
+  let jobInputV = document.querySelector('.popup__input_line_description').value = profileJob.textContent;
+}
+
+function closePopup() {
+  formElement.classList.remove('popup_opened');
+}
+
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+
+// ↑ выскакивание окошка
+
+let popupForm = document.querySelector('.popup__form')
+
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  console.log(nameInput.value);
-  console.log(jobInput.value);
-  // Получите значение полей jobInput и nameInput из свойства value
 
   let profileName = document.querySelector('.profile__name');
   let profileJob = document.querySelector('.profile__description');
@@ -31,19 +35,9 @@ function formSubmitHandler (evt) {
   profileJob.textContent = jobInput.value;
   // Вставьте новые значения с помощью textContent
 
-}
+  closePopup();
 
+}
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
-submitButton.addEventListener('click', function() {
-  formElement.classList.remove('popup_opened');
-});
-
-
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__description');
-let nameInputV = document.querySelector('.popup__input_line_name').value = profileName.textContent;
-let jobInputV = document.querySelector('.popup__input_line_description').value = profileJob.textContent;
-
-// ↑ инпуты заполняются контентом профиля
+popupForm.addEventListener('submit', formSubmitHandler);
