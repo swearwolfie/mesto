5f24e9e05e6a1102be9acf5415e15569930aa098
Masha Soldatova <threebadwolves@gmail.com>
2022-09-21 01:49:43 +0300

    feat: добавить лайки и удаление карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,53 @@
-let formElement = document.querySelector('.popup');
-let editButton = document.querySelector('.profile__edit-button');
-let closeButton = document.querySelector('.popup__close-icon');
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__description');
-let nameInput = document.querySelector('.popup__input_line_name');
-let jobInput = document.querySelector('.popup__input_line_description');
+const formElement = document.querySelector('.popup');
+const editButton = document.querySelector('.profile__edit-button');
+const closeButton = document.querySelector('.popup__close-icon');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__description');
+const nameInput = document.querySelector('.popup__input_line_name');
+const jobInput = document.querySelector('.popup__input_line_description');
+const formEdit = document.querySelector('.popup_edit');
+const formAdd = document.querySelector('.popup_add');
+const addButton = document.querySelector('.profile__add-button');
+const closeAddButton = document.querySelector('.popup__close-icon_add')
+const cardNameInput = document.querySelector('.popup__input_add_name');
+const cardPicInput = document.querySelector('.popup__input_add_pic');
 
-function openPopup() {
-  formElement.classList.add('popup_opened');
+function openEditPopup() {
+  formElement.classList.add('popup_opened'); 
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 
-function closePopup() {
+function closeEditPopup() {
   formElement.classList.remove('popup_opened');
 }
 
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
+editButton.addEventListener('click', openEditPopup);
+closeButton.addEventListener('click', closeEditPopup);
 
-// ↑ выскакивание окошка
+// ↑ выскакивание окошка edit
 
-let popupForm = document.querySelector('.popup__form')
+function openAddPopup() {
+  formAdd.classList.add('popup_opened'); 
+}
+
+function closeAddPopup() {
+  formAdd.classList.remove('popup_opened');
+}
+
+addButton.addEventListener('click', openAddPopup);
+closeAddButton.addEventListener('click', closeAddPopup);
+
+// ↑ выскакивание окошка add
+// ↓ сабмит для редактирования профиля
+
+const popupForm = document.querySelector('.popup__form')
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
 
-  let profileName = document.querySelector('.profile__name');
-  let profileJob = document.querySelector('.profile__description');
+  const profileName = document.querySelector('.profile__name');
+  const profileJob = document.querySelector('.profile__description');
   // Выберите элементы, куда должны быть вставлены значения полей
 
   profileName.textContent = nameInput.value;
@@ -40,3 +60,116 @@ function formSubmitHandler (evt) {
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 popupForm.addEventListener('submit', formSubmitHandler);
+
+// ↓ сабмит для добавления карточек
+
+const cards = document.querySelector('.cards');
+const cardsItemTemplate = document.querySelector('.cards__item-template').content;
+const popupAddForm = document.querySelector('.popup__form_add');
+
+function addNewCard (evt) {
+  evt.preventDefault();
+  const addedCardTemplate = cardsItemTemplate.cloneNode(true);
+  const cardName = addedCardTemplate.querySelector('.cards__name');
+  const cardPic = addedCardTemplate.querySelector('.cards__pic');
+  // элементы, куда вставляются значения
+
+  cardName.textContent = cardNameInput.value;
+  cardPic.src = cardPicInput.value;
+
+  cards.prepend(addedCardTemplate);
+  closeAddPopup();
+
+}
+
+popupAddForm.addEventListener('submit', addNewCard);
+
+// добавляем свои карточки
+
+const initialCards = [
+  {
+    name: 'Национальный Парк Бэдлэндс',
+    link: 'https://i.ibb.co/x6YS8s3/Badlands-National-Park-South-Dakota-USA.jpg'
+  },
+  {
+    name: 'Горы Блу Ридж',
+    link: 'https://i.ibb.co/pj5KYMy/Blue-Ridge-Mountains.jpg'
+  },
+  {
+    name: 'Йеллоустоун',
+    link: 'https://i.ibb.co/MZB5HMW/Yellowstone.jpg'
+  },
+  {
+    name: 'Тропа Дяди Тома',
+    link: 'https://i.ibb.co/0jBFndg/Uncle-Toms-Trail.jpg'
+  },
+  {
+    name: 'Водопад Малтнома',
+    link: 'https://i.ibb.co/QDjSfWy/Multnomah-Falls.jpg'
+  },
+  {
+    name: 'Остров Оаху',
+    link: 'https://i.ibb.co/TkPqyZH/Oahu.jpg'
+  }
+];
+// применяем для каждого элемента
+  initialCards.forEach(function (element) {
+    //клонируем темплейт
+    const jsCards = cardsItemTemplate.cloneNode(true);
+
+    //наполняем
+    jsCards.querySelector('.cards__name').textContent = element.name;
+    jsCards.querySelector('.cards__pic').src = element.link;
+    jsCards.querySelector('.cards__pic').alt = element.name;
+
+  cards.prepend(jsCards);
+});
+
+
+// лайки
+
+const likeButton = Array.from(document.querySelectorAll('.cards__like-button'));
+
+likeButton.forEach(function(button) {
+  button.addEventListener('click', () => {
+    button.classList.toggle('cards__like-button_active');
+  })
+})
+
+// удаление карточек
+
+
+const deleteCardButton = Array.from(document.querySelectorAll('.cards__bin'));
+const cardsItemArr = Array.from(document.querySelectorAll('.cards__item'));
+
+deleteCardButton.forEach(function(button) {
+  button.addEventListener('click', () => {
+    button.parentElement.remove();
+  })
+})
+
+// попап картинки
+
+const fullPicArr = Array.from(document.querySelectorAll('.cards__pic'));
+const fullPicNameArr = Array.from(document.querySelectorAll('.cards__name'));
+const popupPic = document.querySelector('.popup_pic');
+const popupImage = document.querySelector('.popup__image');
+const popupDescription = document.querySelector('.popup__description');
+const closePicButton = document.querySelector('.popup__close-icon_pic');
+
+
+function openPicPopup() {
+  popupPic.classList.add('.popup_opened');
+  popupImage.src = element.closest('.cards__pic').src;
+  popupDescription.textContent = element.closest('.cards__name').textContent;
+}
+
+function closePicPopup() {
+  popupPic.classList.remove('.popup_opened');
+}
+
+fullPicArr.forEach(function(element) {
+  element.addEventListener('click', () => {
+    popupPic.classList.add('.popup_opened');
+  });
+})
