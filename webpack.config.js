f598b012876fb6bbcb9ea0cf9eb3a67466a5e142
Masha Soldatova <threebadwolves@gmail.com>
2022-11-30 00:23:48 +0300

    feat: добавить babel config

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,18 @@ module.exports = {
   devServer: {
     static: './dist',
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 };
