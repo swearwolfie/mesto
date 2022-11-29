f40f020c100f4dd847a2495acb6e83939723846b
Masha Soldatova <threebadwolves@gmail.com>
2022-11-30 00:41:42 +0300

    feat: add HTML configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -22,5 +23,10 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
 };
